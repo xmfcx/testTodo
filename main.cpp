11d1f26f6cfd114f258803401de8a942cdec8811
M. Fatih CIRIT <mfcirit@gmail.com>
2018-05-24 16:47:11 +0300

    but this works

--- main.cpp
+++ main.cpp
@@ -1,15 +1,27 @@
-#include "mainwindow.h"
-#include <QApplication>
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
 
-int main(int argc, char *argv[])
-{
-  QApplication a(argc, argv);
-  MainWindow w;
-  w.show();
+#include "todolist.h"
+#include "todomodel.h"
 
-  return a.exec();
+int main(int argc, char *argv[]) {
+  QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+  QGuiApplication app(argc, argv);
+
+  qmlRegisterType<ToDoModel>("ToDo", 1, 0, "ToDoModel");
+  qmlRegisterUncreatableType<ToDoList>("ToDo", 1, 0, "ToDoList",
+                                       QStringLiteral(
+                                         "ToDoList should not be created in QML"));
+
+  ToDoList toDoList;
+
+  QQmlApplicationEngine engine;
+  engine.rootContext()->setContextProperty(QStringLiteral("toDoList"),
+                                           &toDoList);
+  engine.load(QUrl(QLatin1String("qrc:/main.qml")));
+  if (engine.rootObjects().isEmpty())
+    return -1;
+
+  return app.exec();
 }
-
-
-
-
